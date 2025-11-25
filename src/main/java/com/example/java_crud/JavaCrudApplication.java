92262ec47f7d1598d2ab1318e65eb3ed0e36d3b3
Rajan <109027109+rajan0031@users.noreply.github.com>
2025-11-25 10:47:59 +0000

    addedg

--- src/main/java/com/example/java_crud/JavaCrudApplication.java
+++ src/main/java/com/example/java_crud/JavaCrudApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaCrudApplication {
     public static void main(String[] args) {
+        System.out.println("server is started....");
         SpringApplication.run(JavaCrudApplication.class, args);
     }
 }
